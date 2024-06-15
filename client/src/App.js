dbd2710cfad5e20243ac479bda3ec8d753f59243
dumont, judea <dumontjudea2@gmail.com>
2024-06-14 20:14:49 -0700

    link using react-router-dom Link

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from "./pages/homepage";
 import {Route, Routes} from "react-router-dom";
+import NavBar from "./components/navbar";
 
 function App() {
     return (
         <div>
+            <NavBar></NavBar>
             <Routes>
                 <Route path="/" element={<Homepage/>}></Route>
             </Routes>
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,15 +1,18 @@
-import {AppBar, Box, Toolbar, Typography, Button, Link} from "@mui/material";
+import {AppBar, Box, Toolbar, Typography, Button} from "@mui/material";
+import {Link} from "react-router-dom";
 
 function NavBar(){
     return (
         <Box sx={{flexGrow:1}}>
             <AppBar position={'static'}>
                 <Toolbar>
-                    <Typography>
-                        <Link to={"/"}>ReactLogin</Link>
+                    <Typography variant="h5" component="div">
+                        <Link to="/" style={{textDecoration:'none', color:'white'}}>ReactLogin</Link>
                     </Typography>
                 </Toolbar>
             </AppBar>
         </Box>
     );
 }
+
+export default NavBar;
